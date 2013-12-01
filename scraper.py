cbbb88e0fbf06d77d01a9181f6d2111c7bb401f7
mohamed <mohamed.ibnalkadi@gmail.com>
2013-12-01 09:54:19 +0100

    ajax web pages scraped

--- scraper.py
+++ scraper.py
@@ -1,13 +1,30 @@
+#! /usr/bin/python
 #-*-coding:utf-8 -*-
 
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 import nltk
+import pickle
+import jabba_webkit as jw
 
-def scrap(url):
+url_base="http://skyscanner.fr/"
+
+def scrapAjaxWebpage(url):
+	
+	html=(jw.get_page(url)).encode('ascii','ignore')
+	with open("dataAjax.txt","w") as ajaxdata:
+		ajaxdata.write(html)
+	print html
+	return html
+def scrap(depart,arrival):
+	url=url_base+"transport/vols/"+depart+"/"+arrival+"/"
+	print(url)
 	html=urlopen(url).read()
 	soup=BeautifulSoup(html,"lxml")
-	#context=soup.get_text()
+	context=nltk.clean_html(html)
+	with open("data.txt","w") as scraped_data:
+		mydata=scraped_data.write(url)
+	print(context)        		
 	return soup
 
 def clean_text(url):
@@ -17,17 +34,21 @@ def clean_text(url):
 
 def extract_data(soup):
 
-	data=soup.get_text()
-	return data 
+		data=soup.get_text()
+		return data 
 
 
+	
 
 
 if __name__=="__main__":
-
-	url="http://www.royalairmaroc.com"
-	scrapit=scrap(url)
-	data=extract_data(scrapit)
-	text=clean_text(url)
+        url="http://www.skyscanner.fr/transport/vols/brus/casa/131214/tarifs-de-bruxelles-a-casablanca-en-decembre-2013.html?rtn=0"	
+	url1="http://www.skyscanner.net"
+	depart="cdg"
+	arrival="casa"
+	scrapajax=scrapAjaxWebpage(url)
+	#scrapit=scrap(depart, arrival)
+	#data=extract_data(scrapit)
+	#text=clean_text(url)
 	#print data
-	print text 
+	#print text 
